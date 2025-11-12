a222f93426310d3258207923b21b49f00f1661cc
Joost De Cock <joost@joost.at>
2025-11-12 13:06:27 +0100

    [processors] fix: Do not double-cache events

--- processors/moriohub/events/index.mjs
+++ processors/moriohub/events/index.mjs
@@ -72,6 +72,10 @@ It can cache recent events, and supports dynamic loading of module-specific logi
  */
 function handler (params) {
   const { data, tools, settings } = params
-  if (settings.cache) tools.cache.event(data, settings)
-  if (settings.escalate) escalate(params)
+  /*
+   * Escalation will also cache to detect dupes
+   * So only cache if escaltion is disabled
+   */
+  if (settings.cache && settings.escalate) escalate(params)
+  else if (settings.cache) tools.cache.event(data, settings)
 }
