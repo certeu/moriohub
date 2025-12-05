d04b406c9be63862c3a4e7e4f744141021f399b5
Joost De Cock <joost@joost.at>
2025-12-05 14:34:49 +0100

    [charts]: Added metrics charts for linux-coredns-docker-journald

---
+++ charts/metrics/linux-coredns-docker-journald.mjs
@@ -0,0 +1,154 @@
+export default {
+  memory: ({ data, templates, clone, formatBytes }) => {
+    const chart = {
+      ...clone(templates.charts.line),
+      id: 'memory',
+      series: ['res', 'virt'].map(name => ({
+        ...templates.series.line,
+        name,
+        data: data.map(entry => [entry.timestamp, entry[name]])
+      }))
+    }
+    chart.title.text = 'Memory Usage'
+    chart.yAxis.name = 'Bytes'
+    chart.yAxis.axisLabel = { formatter: formatBytes }
+
+    return [ chart ]
+  },
+  network: ({ data, templates, clone, formatBytes }) => {
+    const chart = {
+      ...clone(templates.charts.line),
+      id: 'memory',
+      series: ['egress', 'ingress'].map(name => {
+        let prev
+        return {
+          ...templates.series.line,
+          name,
+          data: data.map((entry, i) => {
+            let val
+            if (i === 0) val = 0
+            else val = (entry[name] - prev)/30
+            prev = entry[name]
+
+            return [entry.timestamp, val]
+          })
+        }
+      })
+    }
+    /*
+    * Rather than having the first value always be zero,
+    * we set it equal to the second value because that makes
+    * the graph easier to interpret.
+    */
+    for (const i in chart.series) {
+      chart.series[i].data[0] = chart.series[i].data[1]
+    }
+
+    chart.title.text = 'Network'
+    chart.yAxis.name = 'Bps'
+    /*
+    * This is in bytes, but we turn it into bps
+    * Since we divided by 30 earlier it is now bytes per second
+    * so we multiply by 8 and lowercase the B in teh label to get bps
+    */
+    chart.yAxis.axisLabel = { formatter: (val) => formatBytes(val*8, '/s').replace('B', 'b') }
+
+    return [ chart ]
+  },
+  responses: ({ data, templates, clone, formatBytes }) => {
+    // This is a bit more complicated as datasets are per zone/rcodes
+    const zones = new Set()
+    const rcodes = new Set()
+    for (const d of data) {
+      zones.add(d.zone)
+      rcodes.add(d.rcode)
+    }
+
+    // Deal with many-zones/many-rcodes
+    const charts = {}
+    for (const zone of [...zones].sort()) {
+      charts[zone] = {
+        ...clone(templates.charts.line),
+        id: zone,
+        series: [...rcodes].sort().map(rcode => {
+          let prev
+          return {
+            ...templates.series.line,
+            name: rcode,
+            data: data.filter(entry => (entry.zone === zone && entry.rcode === rcode)).map((entry, i) => {
+              let val
+              if (i === 0) val = 0
+              else val = (entry.resps - prev)/30
+              prev = entry.resps
+
+              return [entry.timestamp, val]
+            })
+          }
+        })
+      }
+      charts[zone].title.text = `DNS Responses per second for zone: ${zone}`
+      charts[zone].yAxis.name = 'Resp/s'
+      /*
+      * Rather than having the first value always be zero,
+      * we set it equal to the second value because that makes
+      * the graph easier to interpret.
+      */
+      for (const i in charts[zone].series) {
+        charts[zone].series[i].data[0] = charts[zone].series[i].data[1]
+      }
+    }
+
+    return Object.values(charts)
+  },
+  requests: ({ data, templates, clone, formatBytes }) => {
+    // This is a bit more complicated as datasets are per zone/proto/type
+    const zones = new Set()
+    const protos = new Set()
+    const types = new Set()
+    for (const d of data) {
+      zones.add(d.zone)
+      protos.add(d.proto)
+      types.add(d.type)
+    }
+    if ([...protos].length > 1) {
+      console.log('We do not (currently) support more than one type in the CoreDNS requests metrics')
+      return []
+    }
+
+    // Deal with many-zones/many-types
+    const charts = {}
+    for (const zone of [...zones].sort()) {
+      charts[zone] = {
+        ...clone(templates.charts.line),
+        id: zone,
+        series: [...types].sort().map(type => {
+          let prev
+          return {
+            ...templates.series.line,
+            name: type,
+            data: data.filter(entry => (entry.zone === zone && entry.type === type)).map((entry, i) => {
+              let val
+              if (i === 0) val = 0
+              else val = (entry.reqs - prev)/30
+              prev = entry.reqs
+
+              return [entry.timestamp, val]
+            })
+          }
+        })
+      }
+      charts[zone].title.text = `DNS Requests per second for zone: ${zone}`
+      charts[zone].yAxis.name = 'Req/s'
+      /*
+      * Rather than having the first value always be zero,
+      * we set it equal to the second value because that makes
+      * the graph easier to interpret.
+      */
+      for (const i in charts[zone].series) {
+        charts[zone].series[i].data[0] = charts[zone].series[i].data[1]
+      }
+    }
+
+    return Object.values(charts)
+  },
+}
