3b6bbe1e6d0f82c533bfc71e1e579bbcc0e274fa
Joost De Cock <joost@joost.at>
2025-12-09 16:17:08 +0100

    [processors] feat: Remove cluster_status from linux-elasticsearch

--- processors/moriohub/metrics/linux-elasticsearch/cluster_status.mjs
+++
@@ -1,9 +0,0 @@
-/*
- * Caches cluster_status metricset, and eventifies.
- */
-export default function cluster_status (params) {
-  if (params.settings.cache) {
-    // Default caching
-    //params.tools.cache.metricset({ status: params.data.elasticsearch?.cluster?.stats?.status }, params, 'cluster_status')
-  }
-}
