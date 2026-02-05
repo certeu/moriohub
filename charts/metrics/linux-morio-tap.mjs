57533118a30b8e5636506655145868590e77b478
Joost De Cock <joost.decock@cert.europa.eu>
2026-02-05 15:45:53 +0100

    chore: Work on charts for linux-morio-tap

--- charts/metrics/linux-morio-tap.mjs
+++ charts/metrics/linux-morio-tap.mjs
@@ -1,55 +1,5 @@
 export default {
-  throughput: ({ data, templates, clone }) => {
-    const eps = 'Events per second'
-
-    const topics = {
-      ...clone(templates.charts.line),
-      id: 'topics',
-      series: Object.keys(data[0].data.topics).map(name => ({
-        ...templates.series.line,
-        name,
-        data: data.map(entry => Math.ceil(entry.data.topics[name]/30)),
-      }))
-    }
-    topics.title.text = 'Throughput per topic'
-    topics.yAxis.name = eps
-
-    const procs = {
-      ...clone(templates.charts.line),
-      id: 'processors',
-      series: Object.keys(data[0].data.procs).map(name => ({
-        ...templates.series.line,
-        name,
-        data: data.map(entry => Math.ceil(entry.data.procs[name]/30)),
-      }))
-    }
-    procs.title.text = 'Throughput per stream processor'
-    procs.yAxis.name = eps
-
-    const topicsP = {
-      ...clone(templates.charts.line),
-      id: 'topics.peak',
-      series: Object.keys(data[0].data.peak.topics).map(name => ({
-        ...templates.series.line,
-        name,
-        data: data.map(entry => Math.ceil(entry.data.peak.topics[name]/30)),
-      }))
-    }
-    topicsP.title.text = 'Peak throughput per topic'
-    topicsP.yAxis.name = eps
-
-    const procsP = {
-      ...clone(templates.charts.line),
-      id: 'processors.peak',
-      series: Object.keys(data[0].data.peak.procs).map(name => ({
-        ...templates.series.line,
-        name,
-        data: data.map(entry => Math.ceil(entry.data.peak.procs[name]/30)),
-      }))
-    }
-    procsP.title.text = 'Peak throughput per stream processor'
-    procsP.yAxis.name = eps
-
-    return [topics, procs, topicsP, procsP]
+  json: ({ data, templates, clone }) => {
+    return []
   },
 }
